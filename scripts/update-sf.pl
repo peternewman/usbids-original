a1cfac76d17df2434976beae71e796362f3a8b45
Michal Vaner <vorner@ucw.cz>
2009-02-11 15:47:10 +0100

    Update correctly links with /mods for sourceforge
    Only /read links were updated, /read and /mods are now.

--- scripts/update-sf.pl
+++ scripts/update-sf.pl
@@ -9,7 +9,7 @@ O=~/ids/docs
 cp -aL $O/static .
 cp -aL $O/{v2.2,pci.ids*} .
 sed <$O/index.html >index.html -e '
-	s@href="/read/@href="http://pci-ids.ucw.cz/read/@g
+	s@href="/\(read\|mods\)/@href="http://pci-ids.ucw.cz/\1/@g
 '
 
 rsync -avz --delete ~/sf.net/htdocs mares,pciids@web.sourceforge.net:
